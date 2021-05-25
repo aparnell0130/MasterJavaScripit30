5a1bd027ed58d5ea3874e3ad87cef596abba9b6c
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-05-25 10:24:42 -0700

    added functionallity to move hr hand

--- Clock/index.js
+++ Clock/index.js
@@ -11,6 +11,10 @@ function setDate() {
     const minutesDegrees = ((minutes / 60) * 360) + 90
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
 
+    const hours = now.getHours()
+    const hoursDegrees = ((hours / 12) * 360) + 90
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
 }
 
 setInterval(setDate, 1000)
