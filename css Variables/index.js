3bf8872c7ab712c8c8ca125946350e0cd8c56715
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-05-27 10:56:23 -0700

    added js with event listeners

---
+++ css Variables/index.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate() {
+
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('movemouse', handleUpdate))
